bd28d5a8a8eb007f4a47fadf996f87593f25c93c
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-05-23 15:34:04 -0400

    initial project

---
+++ src/Memento/Editor.java
@@ -0,0 +1,40 @@
+package Memento;
+
+public class Editor {
+
+    private String prevContent;
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * This method allows us to keep track of the previous state
+     * @param content the content from the editor
+     */
+    public void setContent(String content) {
+        prevContent = this.content;
+        this.content = content;
+    }
+
+    public void undo() {
+        this.content = this.prevContent;
+    }
+
+    /**
+     * Will create a new EditorState object to hold the current state
+     * @return a new EditorState with the current content
+     */
+    public EditorState createState() {
+        return new EditorState(content);
+    }
+
+    /**
+     * Resets the state to it's previous
+     * @param state the current state
+     */
+    public void restore(EditorState state) {
+        content = state.getContent();
+    }
+}
